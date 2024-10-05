7ca584c63037af6fc505864b3b4b30245e1e55b1
Aravind P <131600184+araviind-p@users.noreply.github.com>
2024-10-05 22:44:22 +0530

    Time: 142 ms (33.81%), Space: 58.6 MB (32.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,9 +2,6 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    if(x<0){
-        return false
-    }
-     return x.toString() === x.toString().split("").reverse().join("")
+var isPalindrome = function (x) {
+    return x.toString() === x.toString().split("").reverse().join("")
 };
