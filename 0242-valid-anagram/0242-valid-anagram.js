d26de98d92ea56721a3b7f9e0e206831bc2f68a6
Aravind P <131600184+araviind-p@users.noreply.github.com>
2024-10-08 10:30:23 +0530

    Time: 69 ms (64.68%), Space: 51 MB (83.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,9 +3,22 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if(s.length!=t.length){
-        return false
+var isAnagram = function (s, t) {
+    // if(s.length!=t.length){
+    //     return false
+    // }
+    // return s.split("").sort().join("") === t.split("").sort().join("")
+
+    if (s.length != t.length) return false
+
+    let obj1 = {}
+    let obj2 = {}
+    for (let i = 0; i < s.length; i++) {
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1
     }
-    return s.split("").sort().join("") === t.split("").sort().join("")
+    for (const key in obj1) {
+        if (obj1[key] != obj2[key]) return false
+    }
+    return true
 };
