187268f75c5c03672bd7d6d94007c5c485603bc0
Aravind P <131600184+araviind-p@users.noreply.github.com>
2024-10-08 10:21:25 +0530

    Time: 74 ms (25.45%), Space: 48.9 MB (20.71%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -3,13 +3,6 @@
  * @return {number}
  */
 var fib = function (n) {
-    fibArr = [0, 1]
-    if (n < 2) {
-        return fibArr[n]
-    }
-    for (let i = 2; i <= n; i++) {
-        fibArr.push(fibArr[i-1] + fibArr[i - 2])
-        console.log('fibArr: ', fibArr);
-    }
-    return fibArr[n]
-};
+    if (n <= 1) return n
+    return fib(n - 1) + fib(n - 2)
+}
