58bb7bec50cd2451bd2cd2159e9d58fd5eaa2e45
Aravind P <131600184+araviind-p@users.noreply.github.com>
2024-10-13 22:53:22 +0530

    Time: 58 ms (32.1%), Space: 49 MB (54.73%) - LeetHub

---
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} sequence
+ * @param {string} word
+ * @return {number}
+ */
+var maxRepeating = function(sequence, word) {
+    if(!sequence.includes(word)){
+        return 0;
+    }
+    let curr=word
+    let count=0
+    while(sequence.includes(curr)){
+        count++
+        curr+=word
+    }
+    return count
+};
