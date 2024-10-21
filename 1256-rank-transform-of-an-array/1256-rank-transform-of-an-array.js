7051e21913346e84a2d27239decb9c605914e37e
Aravind P <131600184+araviind-p@users.noreply.github.com>
2024-10-21 16:57:00 +0530

    Time: 107 ms (99.6%), Space: 78.9 MB (29.7%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.js
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.js
@@ -3,14 +3,12 @@
  * @return {number[]}
  */
 var arrayRankTransform = function (arr) {
-    const sortedUniqueArr = [...new Set(arr)].sort((a, b) => a - b); // Get sorted unique elements
-    const rankMap = {}; // To store the rank of each element
+    const sortedUniqueArr = [...new Set(arr)].sort((a, b) => a - b);
+    const rankMap = {};
 
-    // Assign rank to each unique element
     for (let i = 0; i < sortedUniqueArr.length; i++) {
         rankMap[sortedUniqueArr[i]] = i + 1;
     }
 
-    // Transform the original array into its rank representation
     return arr.map(num => rankMap[num]);
 };
